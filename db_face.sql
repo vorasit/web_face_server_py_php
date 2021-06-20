4b3565ce96a71899ac6d8f15ac56244449b529de
vorasit phusuwan <40907778+vorasit@users.noreply.github.com>
2021-06-20 20:06:57 +0700

    Update db_face.sql

--- db_face.sql
+++ db_face.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2021 at 08:34 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.0
+-- Generation Time: Jun 20, 2021 at 03:06 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,8 +30,15 @@ SET time_zone = "+00:00";
 CREATE TABLE `check_temp` (
   `p_id` varchar(10) NOT NULL,
   `temp` varchar(10) NOT NULL,
-  `check_in` date NOT NULL
+  `check_in` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `check_temp`
+--
+
+INSERT INTO `check_temp` (`p_id`, `temp`, `check_in`) VALUES
+('PIN', '36', '2021-06-17 15:19:02.598257');
 
 -- --------------------------------------------------------
 
@@ -42,8 +49,16 @@ CREATE TABLE `check_temp` (
 CREATE TABLE `login` (
   `id` int(5) NOT NULL,
   `user_name` varchar(30) NOT NULL,
-  `password` varchar(10) NOT NULL
+  `pwd` varchar(10) NOT NULL,
+  `Userlevel` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `user_name`, `pwd`, `Userlevel`) VALUES
+(2, 'root', '12345678', 'A');
 
 -- --------------------------------------------------------
 
@@ -65,10 +80,22 @@ CREATE TABLE `person` (
 --
 
 --
+-- Indexes for table `check_temp`
+--
+ALTER TABLE `check_temp`
+  ADD PRIMARY KEY (`p_id`);
+
+--
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `person`
+--
+ALTER TABLE `person`
+  ADD PRIMARY KEY (`p_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -78,7 +105,7 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
